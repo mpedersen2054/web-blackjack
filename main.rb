e41483f7c32f7f7691e4bced3c03cf97b4df4503
Matt Pedersen <mpedersen2054@gmail.com>
2013-11-13 20:56:09 -0600

    general routes/templates created | basic styling

--- main.rb
+++ main.rb
@@ -1,10 +1,28 @@
 require 'rubygems'
 require 'sinatra'
+require 'pry'
 
 set :sessions, true
 
-get '/home' do
-  "HELLO WORLD!!"
+get '/' do
+  erb :game_start
 end
 
+post '/game_start' do
+  session[:player_name] = params[:player_name]
+  erb :bet
+end
 
+post '/bet' do
+  session[:loot] = 500
+  session[:current_bet] = params[:current_bet]
+  redirect '/game'
+end
+
+get '/game' do
+  erb :game
+end
+
+get '/game_over' do
+
+end
