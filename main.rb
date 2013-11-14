b251aaae25c8ed82480fee762271e71838bfd8d5
Matt Pedersen <mpedersen2054@gmail.com>
2013-11-14 14:31:57 -0600

    deck and deal cards setup

--- main.rb
+++ main.rb
@@ -21,6 +21,40 @@ end
 
 get '/game' do
   erb :game
+  # initiate deck
+  session[:deck] = []
+  ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'].each do |value|
+    ['Hearts', 'Spades', 'Clubs', 'Diamonds'].each do |suit|
+      session[:deck] << [suit, value]
+    end
+  end
+  session[:deck].shuffle!
+
+  # initiate player/dealer hand
+  session[:player_hand] = []
+  session[:dealer_hand] = []
+
+  # deal cards
+  session[:player_hand] << session[:deck].pop
+  session[:dealer_hand] << session[:deck].pop
+  session[:player_hand] << session[:deck].pop
+  session[:dealer_hand] << session[:deck].pop
+
+  # show flop
+
+
+  # player turn
+
+
+  # dealer turn
+
+
+  # who won?
+
+
+
+
+  binding.pry
 end
 
 get '/game_over' do
